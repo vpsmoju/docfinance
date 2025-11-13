2407d1a3a8802cbea3a2f30799d997a8784231c1
Walkerson Souza <walkerson.cursos@gmail.com>
2025-11-13 00:18:39 -0300

    auth: registrar RequireLoginMiddleware no settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "usuarios.middleware.RequireLoginMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
